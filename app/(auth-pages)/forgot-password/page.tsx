13f656af5dc4e01cd897cc05058f45eb31575034
muqsithinsuci <dimahluthfi15@gmail.com>
2025-08-25 10:35:45 +0700

    fix: reset password error

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -1,37 +1,41 @@
-"use client";
+import { forgotPasswordAction } from "@/app/actions";
+import { FormMessage, Message } from "@/components/form-message";
+import { SubmitButton } from "@/components/submit-button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { SmtpMessage } from "../smtp-message";
 
-import { useFormStatus } from "react-dom";
-import { Button } from "@/components/ui/button";
-import { Loader2 } from "lucide-react";
-
-interface SubmitButtonProps {
-  children: React.ReactNode;
-  pendingText: string;
-  className?: string;
-}
-
-export function SubmitButton({
-  children,
-  pendingText,
-  className,
-}: SubmitButtonProps) {
-  const { pending } = useFormStatus();
+export default async function ForgotPassword(props: {
+  searchParams: Promise<Message>;
+}) {
+  const searchParams = await props.searchParams;
 
   return (
-    <Button
-      type="submit"
-      disabled={pending}
-      className={className}
-      aria-disabled={pending}
-    >
-      {pending ? (
-        <div className="flex items-center justify-center">
-          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-          {pendingText}
+    <>
+      <form
+        action={forgotPasswordAction}
+        className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto"
+      >
+        <div>
+          <h1 className="text-2xl font-medium">Reset Password</h1>
+          <p className="text-sm text-secondary-foreground">
+            Already have an account?{" "}
+            <Link className="text-primary underline" href="/sign-in">
+              Sign in
+            </Link>
+          </p>
         </div>
-      ) : (
-        children
-      )}
-    </Button>
+        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+          <Label htmlFor="email">Email</Label>
+          <Input name="email" placeholder="you@example.com" required />
+          <SubmitButton pendingText="Sending Reset Link...">
+            Reset Password
+          </SubmitButton>
+          <FormMessage message={searchParams} />
+        </div>
+      </form>
+      <SmtpMessage />
+    </>
   );
 }
